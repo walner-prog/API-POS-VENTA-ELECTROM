65cd675c0f9078f9805d57a209fd722680eab950
Walner <walner@example.com>
2025-07-20 22:37:58 -0600

    gggg

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Desestructuramos las variables de entorno correctamente
 const {
   MYSQLHOST,
   MYSQLPORT,
@@ -9,6 +10,14 @@ const {
   MYSQLPASSWORD,
   MYSQLDATABASE
 } = process.env
+
+console.log('🔍 VARIABLES DB: ', {
+  MYSQLHOST,
+  MYSQLPORT,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLDATABASE
+})
 
 if (!MYSQLHOST || !MYSQLPORT || !MYSQLUSER || !MYSQLPASSWORD || !MYSQLDATABASE) {
   throw new Error('❌ Variables MySQL no definidas. Verificá el .env o Railway > Variables')
