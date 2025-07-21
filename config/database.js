46956e4e2f9114c38819c4a2b257bb91a5585e19
Walner <walner@example.com>
2025-07-20 21:27:55 -0600

    Pri

--- config/database.js
+++ config/database.js
@@ -1,17 +1,11 @@
+// db.js
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-  }
-)
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+})
 
 export default sequelize
