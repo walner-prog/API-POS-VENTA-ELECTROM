3270e5af8be2d834eb52774070445e8d2dc84274
Walner <walner@example.com>
2025-07-24 21:38:56 -0600

    cambioshtth

--- validator/producto.schema.js
+++ validator/producto.schema.js
@@ -7,9 +7,10 @@ export const crearProductoSchema = Joi.object({
   categoria_id: Joi.number().integer().required(),
   precio_compra: Joi.number().precision(2).required(),
   precio_venta: Joi.number().precision(2).required(),
-  stock: Joi.number().integer().min(0).required(),
+ // stock: Joi.number().integer().min(0).required(),
   unidad_medida: Joi.string().max(20).optional().allow(null, ''),
   presentacion: Joi.string().max(50).optional().allow(null, ''),
+  
   venta_mayoreo: Joi.boolean().optional(),
   precio_mayoreo: Joi.number().precision(2).optional().allow(null),
   minimo_mayoreo: Joi.number().integer().optional().allow(null)
