19db966b884136305f25c9cb90b280babbf21ddc
Walner <walner@example.com>
2025-07-26 20:40:44 -0600

    agregue un nuevo funcion para optener todos los productos para el punto de venta

--- services/producto.service.js
+++ services/producto.service.js
@@ -349,8 +349,8 @@ export async function obtenerProductoPorIdService(id) {
 export async function obtenerProductosTodos() {
  
   const productos = await Producto.findAll({
-    attributes: ['id', 'nombre', 'codigo_barra', 'precio_venta', 'stock'],
-    order: [['nombre', 'ASC']]
+    attributes: ['id', 'nombre', 'codigo_barra', 'precio_compra', 'precio_venta', 'stock','unidad_medida', 'presentacion'],
+    order: [['id', 'ASC']]
   });
 
   return productos;
