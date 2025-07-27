8f07b81e290deb409b95bf50b8e6c252bbd833bb
Walner <walner@example.com>
2025-07-27 08:24:20 -0600

    agregue nueva funcion services para potener la ventaid para el detalle de venta

--- routes/venta.routes.js
+++ routes/venta.routes.js
@@ -4,7 +4,7 @@ import authMiddleware from '../middlewares/auth.js'
 const router = Router();
 
 router.post('/', authMiddleware, crearVenta);
-router.get('/ventas/:id', authMiddleware,  obtenerDetalleVentaPorId);
+router.get('/:id', authMiddleware,  obtenerDetalleVentaPorId);
 router.post('/cancelar/:id', authMiddleware, cancelarVenta);
 
 export default router;
