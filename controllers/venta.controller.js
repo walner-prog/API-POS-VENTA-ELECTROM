f3707d7d601d0049bdfeb363a007d6fbf83c1bbb
Walner <walner@example.com>
2025-08-05 09:11:00 -0600

    INSTALAE

--- controllers/venta.controller.js
+++ controllers/venta.controller.js
@@ -35,22 +35,20 @@ export async function obtenerDetalleVentaPorId(req, res) {
  
 export const cancelarVenta = async (req, res) => {
   try {
-    await validarCancelacion(req.body);
     const { motivo } = req.body;
     const venta_id = req.params.id; 
 
     const data = await cancelarVentaService(venta_id, motivo, req.usuario.id);
-  //  logger.info(`⚠️ Venta cancelada - ID Venta: ${venta_id} por el usuario ${req.usuario.id} con motivo: "${motivo}"`);
     
     res.json(data);
   } catch (error) {
-    // logger.error(`❌ Error al cancelar venta ID ${req.params.id} por el usuario ${req.usuario.id}: ${error.message}`);
     res.status(error.status || 500).json({
       success: false,
       message: error.message || 'Error interno'
     });
   }
 };
+
 
 export const listarVentasDelDia = async (req, res) => {
   try {
