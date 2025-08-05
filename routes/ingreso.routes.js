4e910b52a20ebac3af52f50ff75f1b0d8817bf9f
Walner <walner@example.com>
2025-08-05 12:24:10 -0600

    AGREGUE EL MODULO DE INGRESOSfx

--- routes/ingreso.routes.js
+++ routes/ingreso.routes.js
@@ -1,16 +1,19 @@
-import express from 'express' 
-const router = express.Router();
-// usar import en lugar de require 
-import { crearIngreso, actualizarIngreso, listarIngresosPorCaja, anularIngreso } from '../controllers/ingreso.controller.js'
+import express from 'express';
+import {
+  crearIngreso,
+  actualizarIngreso,
+  listarIngresosPorCaja,
+  anularIngreso
+} from '../controllers/ingreso.controller.js';
 
-
-import authMiddleware from '../middlewares/auth.js'
+import authMiddleware from '../middlewares/auth.js';
 import soloCreadorOAdmin from '../middlewares/soloCreadorOAdminEliminaEgreso.js';
+
+const router = express.Router();
 
 router.post('/', authMiddleware, crearIngreso);
 router.put('/:id', authMiddleware, actualizarIngreso);
 router.get('/caja/:caja_id', authMiddleware, listarIngresosPorCaja);
 router.put('/anular/:ingreso_id', authMiddleware, anularIngreso);
 
-
-export default router
+export default router;
