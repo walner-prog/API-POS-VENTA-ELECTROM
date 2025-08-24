df4504bf70608b3ef8176fc6b9b3f75df2524efa
Walner <walner@example.com>
2025-08-24 08:59:49 -0600

    ME

--- routes/compra.route.js
+++ routes/compra.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/', authMiddleware, registrarCompra);
 router.get('/', authMiddleware, listarCompras);
 router.get('/:id', authMiddleware, obtenerCompraPorId);
-router.patch("/:id/anular",authMiddleware, eliminarCompra); // Anular compra
+router.patch("/:id/anular",authMiddleware, eliminarCompra); // Anular compra OK
 
 
 export default router;
